99f69c3f4f15dbbd022245a371651a72cf84c194
felixNT0 <tsowafelix0@gmail.com>
2023-09-26 11:01:25 +0100

    feat: fix the paperlink service menu

--- src/pages/paperdazlink/components/paperlink-service-menu/card/index.tsx
+++ src/pages/paperdazlink/components/paperlink-service-menu/card/index.tsx
@@ -28,17 +28,17 @@ function Card({
   return (
     <div
       style={style}
-      className={`relative rounded-lg border ${cardColor} border-2 p-3 m-3 max-xsm:w-[250px] w-[350px] h-[220px] lg:h-[270px] flex flex-col justify-between max-md:h-auto max-xsm: mt-5`}
+      className={`relative rounded-lg border ${cardColor} border-2 p-3 m-3 max-xsm:w-[250px] w-[450px] h-[220px] flex flex-col justify-around max-md:h-auto max-xsm:mt-5`}
     >
       <div
-        className={`absolute -left-[0.45rem] top-24 max-md:top-16 bottom-0 w-3 h-20 max-sm:top-10 ${bgColor} rounded-lg`}
+        className={`absolute -left-[0.45rem] top-16 max-md:top-16 bottom-0 w-3 h-20 max-sm:top-10 ${bgColor} rounded-lg`}
       />
       <div className="flex justify-between items-center">
-        <h1 className="text-3xl max-md:text-2xl max-sm:text-lg font-bold">
+        <h1 className="text-xl max-md:text-lg max-sm:text-[0.7rem] font-bold">
           {title}
         </h1>
         <h1
-          className={`text-5xl max-md:text-2xl max-sm:text-lg font-bold ${textColor}`}
+          className={`text-xl max-md:text-lg max-sm:text-[0.7rem] font-bold ${textColor}`}
         >
           {number}
         </h1>
@@ -54,7 +54,7 @@ function Card({
           <p className={`text-[0.8rem] ${textColor}`}>{charges}</p>
         </div>
         <div className="flex gap-1 items-center">
-          <h1 className="text-5xl max-md:text-2xl max-sm:text-lg font-bold">
+          <h1 className="text-xl max-md:text-lg max-sm:text-[0.7rem] font-bold">
             {price}
           </h1>
           <p className="max-md:text-md max-sm:text-sm">{type}</p>
